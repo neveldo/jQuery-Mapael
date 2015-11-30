cd961b1d2f9e03259ae011f25709fbc7753ad484
Indigo744 <Indigo744@users.noreply.github.com>
2015-11-30 11:40:36 +0100

    UT: Split test-area into 2 subtests

--- test/test-areas.js
+++ test/test-areas.js
@@ -33,32 +33,91 @@ $(function() {
                 }
             }
         };
-        var CST_CUSTOMAREA = {
-            "department-56": {
-                text: {content: "TEXT_department-56", attrs: {"font-size": 10}},
-                tooltip: {content: "TOOLTIP_department-56"}
-            },
-            "department-21": {
-                attrs: {
-                    fill: "#488402"
-                }
-                , attrsHover: {
-                    fill: "#a4e100"
-                }
-            }
-        };
         
         $(".mapcontainer").mapael({
             map: {
                 name: "france_departments",
                 defaultArea: CST_DEFAULTAREA
+            }
+        }); 
+        
+        assert.ok($(".mapcontainer .map svg")[0], "Map created" );
+        
+        $(".mapcontainer svg path").each(function(id, elem) {
+            var $elem = $(elem);
+            var data_id = $elem.attr("data-id");
+            
+            assert.equal($elem.attr("fill"), CST_DEFAULTAREA.attrs.fill, "Check overriden fill before mouseover for " + data_id);
+            assert.equal($elem.attr("stroke"), CST_DEFAULTAREA.attrs.stroke, "Check overriden stroke before mouseover for " + data_id);
+            
+            $elem.trigger("mouseover");
+            setTimeout(function() {
+                
+                assert.equal($elem.attr("fill"), CST_DEFAULTAREA.attrsHover.fill, "Check overriden hover fill after mouseover for " + data_id);
+                assert.equal($elem.attr("stroke"), CST_DEFAULTAREA.attrsHover.stroke, "Check overriden hover stroke after mouseover for " + data_id);
+                
+                $elem.trigger("mouseout");
+                setTimeout(function() {
+                    assert.equal($elem.attr("fill"), CST_DEFAULTAREA.attrs.fill, "Check overriden fill after mouseout for " + data_id);
+                    mouseover_async_done();
+                }, 500);
+            }, 500);
+        });
+    });
+    
+    test("Area custom option override", function(assert) {
+        var mouseover_async_done = assert.async(CST_NB_OF_FRANCE_DPTMT);
+        var text_mouseover_async_done = assert.async();
+        
+        var CST_CUSTOMAREA = {
+            "department-56": {
+                text: {
+                    content: "TEXT_department-56", 
+                    attrs: {"font-size": 5}, 
+                    attrsHover: {"font-size": 20}
+                },
+                tooltip: {content: "TOOLTIP_department-56"}
+            },
+            "department-21": {
+                attrs: {
+                    fill: "#488402"
+                }, 
+                attrsHover: {
+                    fill: "#a4e100"
+                }
+            },
+            "department-74": {
+                text: {
+                    content: "TEXT_department-74", 
+                    position:"top",
+                    margin:0
+                },
+                href: "http://path.to.74.com",
+                target: "_blank"
+            }
+        };
+        
+        $(".mapcontainer").mapael({
+            map: {
+                name: "france_departments"
             },
             areas: CST_CUSTOMAREA
         }); 
         
         assert.ok($(".mapcontainer .map svg")[0], "Map created" );
         
-        assert.ok($(".mapcontainer .map svg text[data-id='department-56']")[0], "Text created for department-56" );
+        var $text_56 = $(".mapcontainer .map svg text[data-id='department-56']");
+        var $text_74 = $(".mapcontainer .map svg text[data-id='department-74']");
+        
+        assert.ok($text_56[0], "Text created for department-56" );
+        assert.equal($text_56.attr("font-size"), CST_CUSTOMAREA["department-56"].text.attrs["font-size"] + "px", "Font-size ok for department-56" );
+        $text_56.trigger("mouseover");
+        setTimeout(function() {
+            assert.equal($text_56.attr("font-size"), CST_CUSTOMAREA["department-56"].text.attrsHover["font-size"] + "px", "Font-size hover ok for department-56" );
+            text_mouseover_async_done();
+        }, 500);
+        
+        assert.ok($text_74[0], "Text created for department-74" );
         
         $(".mapcontainer svg path").each(function(id, elem) {
             var $elem = $(elem);
@@ -66,39 +125,26 @@ $(function() {
             
             if (data_id === "department-21") {
                 assert.equal($elem.attr("fill"), CST_CUSTOMAREA[data_id].attrs.fill, "Check special overriden fill before mouseover for " + data_id);
-            } else {
-                assert.equal($elem.attr("fill"), CST_DEFAULTAREA.attrs.fill, "Check overriden fill before mouseover for " + data_id);
+            } else if (data_id === "department-74") {
+                /* Can't check more than that since href and target values are handled by onClick event... */
+                assert.ok(jQuery._data( elem, "events" )["click"], "Link created for " + data_id);
             }
-            assert.equal($elem.attr("stroke"), CST_DEFAULTAREA.attrs.stroke, "Check overriden stroke before mouseover for " + data_id);
             
             $elem.trigger("mouseover");
             setTimeout(function() {
                 
                 if (data_id === "department-21") {
                     assert.equal($elem.attr("fill"), CST_CUSTOMAREA[data_id].attrsHover.fill, "Check special overriden hover fill after mouseover for " + data_id);
-                } else {
-                    assert.equal($elem.attr("fill"), CST_DEFAULTAREA.attrsHover.fill, "Check overriden hover fill after mouseover for " + data_id);
-                }
-                assert.equal($elem.attr("stroke"), CST_DEFAULTAREA.attrsHover.stroke, "Check overriden hover stroke after mouseover for " + data_id);
-                
-                if (data_id === "department-56") {
+                } else if (data_id === "department-56") {
                     assert.ok($(".mapcontainer .map .mapTooltip").is( ":visible" ), "Check tooltip visible for " + data_id);
                     assert.equal($(".mapcontainer .map .mapTooltip").html(), CST_CUSTOMAREA[data_id].tooltip.content, "Check special tooltip content for " + data_id);
-                } else {
-                    /* We can't test the following because it will fail for area around dptmt 56... */
-                    //assert.ok($(".mapcontainer .map .mapTooltip").is( ":hidden" ), "Check tooltip hidden for " + data_id);
-                    //assert.equal($(".mapcontainer .map .mapTooltip").html(), "", "Check empty tooltip content for " + data_id);
                 }
                 
                 $elem.trigger("mouseout");
                 setTimeout(function() {
                     if (data_id === "department-21") {
                     assert.equal($elem.attr("fill"), CST_CUSTOMAREA[data_id].attrs.fill, "Check special overriden fill after mouseout for " + data_id);
-                    } else {
-                    assert.equal($elem.attr("fill"), CST_DEFAULTAREA.attrs.fill, "Check overriden fill after mouseout for " + data_id);
-                    }
-                    
-                    if (data_id === "department-56") {
+                    } else if (data_id === "department-56") {
                         assert.ok($(".mapcontainer .map .mapTooltip").is( ":hidden" ), "Check tooltip hidden after mouseout for " + data_id);
                     }
                     
